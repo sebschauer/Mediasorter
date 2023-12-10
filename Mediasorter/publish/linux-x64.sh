68e1dfb2469c59aea973d19ba6f200789c171b1d
sk <sk@noreply.localhost>
2023-12-10 20:36:31 +0100

    install option in publishing scripts (#3)
    Co-authored-by: Sebastian Schauer <git@sebschauer.de>
    Reviewed-on: http://odroid:2544/sk/Mediasorter/pulls/3

--- Mediasorter/publish/linux-x64.sh
+++ Mediasorter/publish/linux-x64.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
 dotnet publish ../Mediasorter.csproj --configuration Release --self-contained --runtime linux-x64
 
+if [ "$1" == "--install" ]
+then
+	echo "Copying mediasorter to /usr/local/bin. Need superuser rights for that."
+	sudo rm /usr/local/bin/mediasorter
+	sudo cp ../bin/Release/net6.0/linux-x64/publish/Mediasorter /usr/local/bin/mediasorter
+	echo "Done."
+fi
